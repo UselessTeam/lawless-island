e91ef7672febefbde3174de7e9d3a72dee93c0ee
cydouzo <cydouzo@gmail.com>
2019-10-07 01:13:23 +0900

    Repair broken commit

--- Assets/Scripts/InteractableBehaviors/RamassableBehavior.cs
+++ Assets/Scripts/InteractableBehaviors/RamassableBehavior.cs
@@ -6,7 +6,10 @@ public class RamassableBehavior : Interactable
 {
 	public ItemType item;
 	public int amount;
+
+    public bool isRequirement = false;
     public ItemType requirement;
+    public SpriteRenderer windowRenderer;
 
     // Start is called before the first frame update
     void Start()
@@ -16,16 +19,18 @@ public class RamassableBehavior : Interactable
 
     protected override void ShowWindow()
     {
+        if (isRequirement)
+        {
+            if (!InventoryHandler.instance.IsEnough(requirement, 1))
+            {
+                windowRenderer.color = Color.red;
+            }
+            else
+            {
+                windowRenderer.color = Color.white;
+            }
+        }
         base.ShowWindow();
-
-        if (!InventoryHandler.instance.IsEnough(requirement, 1))
-        {
-            GetComponentInChildren<SpriteRenderer>().color = Color.red;
-        }
-        else
-        {
-            GetComponentInChildren<SpriteRenderer>().color = Color.white;
-        }
     }
 
     // Update is called once per frame
