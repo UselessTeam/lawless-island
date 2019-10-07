c6471978d8b9c28df7713bbfa6603120356d9214
cydouzo <cydouzo@gmail.com>
2019-10-08 00:08:23 +0900

    Added Menu

---
+++ Assets/Scripts/Menu/MenuSelection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSelection : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("CydouzoScene");
+    }
+
+    public void ShowCredits()
+    {
+        transform.parent.Find("Credits").gameObject.SetActive(true);
+        gameObject.SetActive(false);
+    }
+
+    public void ShowMenu() {
+        transform.parent.Find("MainMenu").gameObject.SetActive(true);
+        gameObject.SetActive(false);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
